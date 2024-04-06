89aae509bae8d6d430889665784b35f8313462f0
rsi2024 <164152459+rsi2024@users.noreply.github.com>
2024-04-06 21:27:50 +0300

    (new_zoo) zoo.py

--- zoo.py
+++ zoo.py
@@ -43,7 +43,7 @@ class Reptile(Animal):
         self.habitat = habitat
 
     def make_sound(self):
-        print(f"{self.name} says hiss")
+        print(f"{self.name} издает звук {self.sound}")
 
     def eat(self):
         print(f"{self.name} ест {self.food}")
@@ -51,17 +51,42 @@ class Reptile(Animal):
     def habitat(self):
         print(f"{self.name} обитает в {self.habitat}")
 
+def animal_sound(animals):
+    for animal in animals:
+        animal.make_sound()
+
+animals = [Bird("Tweety", 2, "щебет", "крошки"),
+           Mammal("Doggy", 5, "гав", "рыжий", "мясо"),
+           Reptile("Snakey", 3, "шипение", "моллюски", "лес")]
+
+animal_sound(animals)
 
 
+class Zoo():
+    def __init__(self):
+        self.animals = []
+        self.staff = []
+
+    def add_animal(self, animal):
+        self.animals.append(animal)
+        print(f"Животное {animal.name} успешно добавлен в зоопарк")
+
+    def add_staff(self, staff):
+        self.staff.append(staff)
+        print(f"Сотрудник {staff.name} успешно добавлен в штат работников зоопарка")
 
 
+class Employee():
+    def __init__(self, name):
+        self.name = name
 
+class ZooKeeper(Employee):
+    def feed_animal(self, animal):
+        print(f"{self.name} кормит животное {animal.name}")
 
-
-
-
-
-
+class Veterinarian(Employee):
+    def hear_animal(self, animal):
+        print(f"{self.name} лечит животное {animal.name}")
 
 
 bird = Bird("Tweety", 2, "щебет", "крошки")
@@ -75,4 +100,34 @@ mammal.make_sound()
 mammal.eat()
 reptile.make_sound()
 reptile.eat()
+
+zoo = Zoo()
+zoo.add_animal(Mammal("Лошадь", 4, "иго-го", "рыжий", "овес"))
+zoo.add_staff(ZooKeeper("Вася"))
+zoo.add_staff(Veterinarian("Петя"))
+
+zoo.animals[0].make_sound()
+zoo.staff[0].feed_animal(zoo.animals[0])
+zoo.staff[1].hear_animal(zoo.animals[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
